84e30bd419e860641083ada95e2dbec437c30dc5
JackGarza04 <97976283+JackGarza04@users.noreply.github.com>
2022-01-26 09:53:51 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
 	});
+	$("#p1").click(function() {
+		$(".disappear").hide(); //Makes all elements with the disappear class disappear
+	});
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
+	});
+	$("img").click(function() {
+		var img = $("img");
+		img.animate({height: '600px', opacity: '0.5'}, "fast");
+		img.animate({width: '600px', opacity: '1'}, "slow");
+		img.animate({height: '200px', opacity: '0.5'}, "fast");
+		img.animate({width: '200px', opacity: '1'}, "slow", function(){ $(this).removeAttr('style');}); //Callback to reset image
 	});
 });
