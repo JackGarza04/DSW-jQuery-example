a806f96da0163b9f2815a4587c79bc6896e356f7
P1 <jack@garzafamily.org>
2022-01-27 09:54:41 -0800

    added to js

--- myscript.js
+++ myscript.js
@@ -3,7 +3,20 @@ $(document).ready(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
 	});
+	$("#p1").click(function() {
+		$(".disappear").hide(); //Makes all elements with the disappear class disappear
+	});
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
+	});
+	$("img").click(function() {
+		var img = $("img");
+		img.animate({left: '+=500'});
+		img.animate({height: '600px'}, "slow");
+		img.animate({height: '200px'}, "slow");
+		img.animate({width: '200px'}, "fast", function(){ $(this).removeAttr('style');}); //Callback to reset image
+	});
+	$("#flip").click(function(){
+		$("#panel").slideDown("slow").toggle();
 	});
 });
